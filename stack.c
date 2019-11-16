464681c6ce4bc7574a3e073ff3d4adbf0dbe43af
sumalata2000 <55432033+sumalata2000@users.noreply.github.com>
2019-11-16 22:39:43 +0530

    Add files via upload

--- stack.c
+++ stack.c
@@ -1,69 +1,88 @@
-#include<stdio.h>
-#include<stdlib.h>
-#define MAX 10
+#include <stdio.h>
+#include <stdlib.h>
 
-int top=-1,stack[MAX];
-void push();
-void pop();
-void display();
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node* Node;
+
+Node push(int ele, Node top)
+{
+    if (top == NULL)
+    {
+        top =(Node)malloc(sizeof(struct node));
+        top->next = NULL;
+        top->data = ele;
+	return top;
+    }
+    else
+    {
+        Node temp =(Node)malloc(sizeof(struct node));
+        temp->next = top;
+        temp->data = ele;
+        top = temp;
+	return top;
+    }
+}
+void display(Node top)
+{
+    Node t = top;
+
+    if (t == NULL)
+    {
+        printf("Stack is empty");
+        return;
+    }
+    printf(" Elements in the stack are:\n");
+    while (t != NULL)
+    {
+        printf("%d ", t->data);
+        t = t->next;
+    }
+ }
+Node pop(Node top)
+{
+    Node t = top;
+
+    if (t == NULL)
+    {
+        printf("\nStack underflow");
+        return top;
+    }
+    else
+        t = t->next;
+    printf("\n Popped element : %d", top->data);
+    free(top);
+    top = t;
+    return top;
+}
 
 void main()
 {
-	int ch;
-	printf("\n STACK MENU");
-	printf("\n 1.push \t 2.pop \t 3.display \t 4.exit");
-	while(ch!=4)
+	int no, ch, e;
+	Node top = NULL;
+	while (1)
 	{
-	printf("\n enter your choice(1-4)");
-	scanf("%d",&ch);
-	switch(ch)
-	{
-	case 1: push();
-		break;
-	case 2: pop();
-		break;
-	case 3: display();
-		break;	
-	case 4: exit(0);
-	default:printf("\n INVALID CHOICE");
-	}
-	}
+		printf("\n 1.Push\n 2.Pop\n 3.Display\n 4.Exit");
+		printf("\n Enter choice : ");
+		scanf("%d", &ch);
 
-}
-void push()
-{
-	int val;
-	if(top==MAX-1)
-	printf("\n ERROR: STACK OVERFLOW");
-	else
-	{
-	printf("\n ENTER THE ELEMENT TO PUSH:");
-	scanf("%d", &val);
-	top=top+1;
-	stack[top]=val;
-	}
-}
-
-void pop()
-{
-	if(top==-1)
-	printf("\n ERROR:STACK UNDERFLOW");
-	else
-	{
-	printf("\n the popped out element is %d", stack[top]);
-	top=top-1;
-}
-}
-
-void display()
-{	
-	int i;
-	if(top==-1)
-	printf("ERROR: STACK IS EMPTY");
-	else
-	{
-	printf("\n stack is \n");
-	for(i=0;i<=top;i++)
-	printf("%d\n", stack[i]);
-	}
+		switch (ch)
+		{
+			case 1:printf(" Enter data : ");
+			       scanf("%d", &no);
+			       top = push(no, top);
+			       break;
+			case 2:top = pop(top);
+			       break;
+			case 3:display(top);
+			       break;
+			case 4:  exit(0);
+			default :printf("Invalid Input");
+				 break;
+		}
+	}	
 }
